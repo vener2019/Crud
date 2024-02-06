0a93665625ffb80f9463b470bf9842f8bdffc25f
vener dollisen <kulotsivener13@gmail.com>
2024-02-06 16:51:35 +0800

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(Request $request){
+        $keyword = $request->get('search');
+        $perPage = 5;
+
+        if(!empty($keyword)){
+            $products = Product::where('name', 'LIKE', "%$keyword%")
+                        ->orWhere('category', 'LIKE', "%$keyword%")
+                        ->latest()->paginate($perPage);
+        }else{
+            $products = Product::latest()->paginate($perPage);
+        }
+        
+        return view('products.index',['products' => $products])->with('i', (request()->input('page', 1) - 1) *5);
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+
+    public function store(Request $request){
+
+        $request->validate([
+            'name' => 'required',
+            'image' => 'required|image|mimes:jpg,png,jpeg,svg|max:2028',
+        ]);
+        $product = new Product;
+
+        $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+        request()->image->move(public_path('images'), $file_name);
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->image = $file_name;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+
+        $product->save();
+
+        return redirect()->route('products.index')->with('success','Product Added successfully');
+    }
+
+    public function edit($id){
+        $product = Product::findOrFail($id);
+        return view('products.edit', ['product' => $product]);
+    }
+
+    public function update(Request $request, Product $product){
+        $request->validate([
+            'name' => 'required',
+        ]);
+        $file_name = $request->hidden_product_image;
+        if($request->image != ''){
+            $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+            request()->image->move(public_path('images'), $file_name);
+        }
+        
+        $product = Product::find($request->hidden_id);
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->image = $file_name;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+
+        $product->save();
+        return redirect()->route('products.index')->with('success','Product Updated successfully');
+    }
+
+    public function destroy($id){
+        $product = Product::findOrFail($id);
+        $image_path = public_path()."iamges/";
+        $image = $image_path. $product->image;
+        if(file_exists($image)){
+            @unlink($image);
+        }
+
+        $product->delete();
+
+        return redirect()->route('products.index')->with('success','Product Deleted successfully');
+    }
+}
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+@section('content')
+    <main class="container">
+        <section>
+            <form method="post" action="{{ route('products.store') }}" enctype="multipart/form-data">
+                @csrf
+                <div class="titlebar">
+                    <h1>Add Product</h1>
+                    <button>Save</button>
+                </div>
+                @if ($errors->any())
+                    <div>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <div class="card">
+                <div>
+                        <label>Name</label>
+                        <input type="text" name="name">
+                        <label>Description (optional)</label>
+                        <textarea cols="10" rows="5" name="description"></textarea>
+                        <label>Add Image</label>
+                        <img src="" alt="" class="img-product" id="file-preview" />
+                        <input type="file" name="image" accept="image/*" onchange="showFile(event)" >
+                    </div>
+                <div>
+                        <label>Category</label>
+                        <select  name="category" id="" >
+                            @foreach (json_decode('{"Smartphone":"Smartphone","Smart TV":"Smart TV","Computer":"Computer"}',true) as $optionKey => $optionVal)
+                            <option value="{{ $optionKey }}" >{{ $optionVal }}</option>
+                            @endforeach
+                            
+                        </select>
+                        <hr>
+                        <label>Inventory</label>
+                        <input type="text" class="input" name="quantity">
+                        <hr>
+                        <label>Price</label>
+                        <input type="text" class="input" name="price">
+                </div>
+                </div>
+                <div class="titlebar">
+                    <h1></h1>
+                    <button>Save</button>
+                </div>
+            </form>
+        </section>
+    </main>
+    <script>
+        function showFile(event){
+            var input = event.target;
+            var reader = new FileReader();
+
+            reader.onload = function(){
+                var dataUrl = reader.result
+                var outout = document.getElementById('file-preview')
+
+                outout.src = dataUrl;
+            };
+
+            reader.readAsDataURL(input.files[0])
+        }
+    </script>
+@endsection
